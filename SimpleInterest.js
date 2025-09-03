59d5144b957373d804074d63c009017840b675bb
Krutika Chincholkar <118680316+KrutikaChincholkar@users.noreply.github.com>
2025-09-03 11:46:11 +0530

    Create SimpleInterest.js
    Module 4: lab1 JS file
    Hands-On Lab: Simple Interest Calculator

---
+++ SimpleInterest.js
@@ -0,0 +1,30 @@
+function updateRate() {
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText = rateval;
+}
+
+function compute() {
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    // Validation for principal
+    if (principal <= 0) {
+        alert("Enter a positive number");
+        document.getElementById("principal").focus();
+        return;
+    }
+
+    var interest = principal * years * rate / 100;
+    var amount = parseInt(principal) + parseFloat(interest);
+    var result = document.getElementById("result");
+
+    // Calculate future year
+    var year = new Date().getFullYear() + parseInt(years);
+
+    // Show result
+    result.innerHTML = "If you deposit $" + "<mark>" + principal + "</mark>" + ",\<br\>" +
+        "at an interest rate of " + "<mark>" + rate + "%" + "</mark>" + ".\<br\>" +
+        "You will receive an amount of $" + "<mark>" + amount + "</mark>" + ",\<br\>" +
+        "in the year " + "<mark>" + year + "</mark>" + "\<br\>";
+}
